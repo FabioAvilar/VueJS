5158b869bacc72d52bc23f87b00cd4a726b2bbaf
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-05 11:52:50 +0200

    course of VueJS

---
+++ VueJS1/src/app.js
@@ -0,0 +1,24 @@
+
+const myNameApp = {
+    data() {
+        return {
+            name: "",
+            age:  30,
+            input_name: ""
+        }
+    },
+    methods: {
+      submitForm(e) {
+
+        e.preventDefault();
+
+        console.log(this.input_name);
+
+        this.name = this.input_name;
+
+      }
+    }
+}
+
+Vue.createApp(myNameApp).mount("#app");
+
