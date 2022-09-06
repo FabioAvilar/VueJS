7e3bfd7acc1ac427c1c8fb1cfe3f74471f9bc88b
FabioBassani <avilar.fabio@yahoo.com.br>
2022-09-06 11:10:21 +0200

    second day of studies

---
+++ 2_projeto-vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <primeiro-componente />
+    <p>O dado de app é: {{testando}}</p>
+    <LifeCycle/>
+  </div>
+</template>
+
+<script>
+  import PrimeiroComponente from './components/PrimeiroComponente.vue';
+  import lifeCycle from './components/lifeCycle.vue';
+import LifeCycle from './components/lifeCycle.vue';
+
+  export default {
+    name: 'App',
+    components: {
+    PrimeiroComponente,
+    lifeCycle,
+    LifeCycle
+},
+data() {
+  return {
+    testando: "Teste"
+  }
+}
+  }
+</script>
